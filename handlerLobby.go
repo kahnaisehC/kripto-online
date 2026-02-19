7fc7767b4b38172d9e4319d15e8836619ca270a3
Ian Gabriel Hakanson <gabianhak@gmail.com>
2026-02-19 01:43:32 -0300

    feat: decoupled api handler functions

--- handlerLobby.go
+++ handlerLobby.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/coder/websocket"
 )
+
+const MaxLobbySize = 32
 
 var _counter ID = 1
 
@@ -29,31 +30,67 @@ type Lobby struct {
 	Link    string
 	AdminID ID
 	Conn    []Connection
+	Size    int
+}
+type LobbyResponse struct {
+	ID      ID
+	Name    string
+	AdminID ID
+	Link    string
+	Players map[ID]string
+	Size    int
 }
 
 func (cfg *config) handlerGetLobbies(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	data, err := json.Marshal(cfg.lobbies)
+	lobbiesData := make([]LobbyResponse, len(cfg.lobbies))
+	for i := range cfg.lobbies {
+		lobbiesData[i].ID = cfg.lobbies[i].ID
+		lobbiesData[i].Name = cfg.lobbies[i].Name
+		lobbiesData[i].AdminID = cfg.lobbies[i].AdminID
+		lobbiesData[i].Link = cfg.lobbies[i].Link
+		lobbiesData[i].Size = cfg.lobbies[i].Size
+		for _, con := range cfg.lobbies[i].Conn {
+			lobbiesData[i].Players[con.userID] = con.UserName
+		}
+	}
+
+	data, err := json.Marshal(lobbiesData)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	w.Header().Add("Content-Type", "application/json")
 	respondWithJSON(w, http.StatusOK, data)
 }
 
 func (cfg *config) handlerGetLobby(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	w.Header().Add("Content-Type", "text/html")
-	err := cfg.temp.ExecuteTemplate(w, "lobby", nil)
+	lobbyIDString := r.PathValue("gameID")
+	lobbyIDInt, err := strconv.Atoi(lobbyIDString)
 	if err != nil {
-		log.Println("its mee")
-		log.Println(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		respondWithError(w, http.StatusBadRequest, err)
 		return
 	}
+	lobbyID := ID(lobbyIDInt)
+
+	lobbyData := LobbyResponse{
+		ID:      cfg.lobbies[ID(lobbyID)].ID,
+		Name:    cfg.lobbies[ID(lobbyID)].Name,
+		AdminID: cfg.lobbies[lobbyID].AdminID,
+		Link:    cfg.lobbies[lobbyID].Link,
+		Size:    cfg.lobbies[lobbyID].Size,
+	}
+	for _, con := range cfg.lobbies[lobbyID].Conn {
+		lobbyData.Players[con.userID] = con.UserName
+	}
+	data, err := json.Marshal(lobbyData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, data)
 }
 
 var _lobbyID ID
@@ -68,13 +105,13 @@ func (cfg *config) handlerPostLobby(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 
-	adminIdCookie, err := r.Cookie("userID")
+	adminIDCookie, err := r.Cookie("userID")
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, errors.New("have to login first"))
 		return
 	}
 
-	adminId, err := strconv.Atoi(adminIdCookie.Name)
+	adminID, err := strconv.Atoi(adminIDCookie.Name)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, errors.New("malformed id. have to login again"))
 		return
@@ -85,18 +122,32 @@ func (cfg *config) handlerPostLobby(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, errors.New("not found lobby name"))
 		return
 	}
+	lobbySizeString := r.FormValue("lobbySize")
+	lobbySize, err := strconv.Atoi(lobbySizeString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, errors.New("not found lobby name"))
+		return
+	}
+	if lobbySize >= MaxLobbySize || lobbySize < 2 {
+		respondWithError(w, http.StatusBadRequest, errors.New("invalid lobby size"))
+		return
+	}
+	lobbyID := newLobbyID()
+	lobbyURL := "/lobby/" + strconv.Itoa(int(lobbyID))
 
 	newLobby := Lobby{
-		ID:      newLobbyID(),
+		ID:      lobbyID,
 		Name:    lobbyName,
-		Link:    "",
-		AdminID: ID(adminId),
+		Link:    lobbyURL,
+		AdminID: ID(adminID),
 		Conn:    nil,
+		Size:    lobbySize,
 	}
-
+	data, err := json.Marshal(newLobby)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err)
+		return
+	}
 	cfg.lobbies = append(cfg.lobbies, newLobby)
-
-	newLobbyData, _ := json.Marshal(newLobby)
-
-	respondWithJSON(w, http.StatusCreated, newLobbyData)
+	respondWithJSON(w, http.StatusCreated, data)
 }
